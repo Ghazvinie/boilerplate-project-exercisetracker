03cd0b38e792c4754545847b7e5d088ef894e88c
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-24 21:40:14 +0100

    added postNewExercise function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const UserModel = require('../models/userModel');
 const handleErrors = require('../utils/handleErrors');
+const mongoose = require('mongoose');
 
-async function postNewUser (req, res) {
+async function postNewUser(req, res) {
     const username = req.body.username.trim();
 
     await UserModel.create({ userName: username })
@@ -18,18 +19,37 @@ async function postNewUser (req, res) {
 async function getUsers(req, res) {
 
     await UserModel.find()
-    .then(collection => {
-        const userArray = collection.reduce((acc, curr) => {
-            acc.push({username: curr.userName, _id: curr._id});
-            return acc;
-        },[]);
-       return res.json(userArray);
-    })
-    .catch(error => console.log(error));
+        .then(collection => {
+            const userArray = collection.reduce((acc, curr) => {
+                acc.push({ username: curr.userName, _id: curr._id });
+                return acc;
+            }, []);
+            return res.json(userArray);
+        })
+        .catch(error => console.log(error));
 }
 
-function postNewExercise(req, res) {
-    res.send('postNewExercise');
+async function postNewExercise(req, res) {
+
+    const _id = req.params._id.trim();
+    const exercise = {
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date || new Date().toDateString()
+    };
+
+    await UserModel.findOneAndUpdate({ _id }, { $push: { exercises: exercise } }, { new: true, useFindAndModify: false })
+        .then(document => {
+            res.json({
+                _id: document._id,
+                date: exercise.date,
+                duration: exercise.duration,
+                description: exercise.description,
+                username: document.userName
+            });
+        })
+        .catch(error => console.log(error));
+
 }
 
 function getUserLogs(req, res) {
@@ -38,3 +58,4 @@ function getUserLogs(req, res) {
 }
 
 module.exports = { postNewUser, getUsers, postNewExercise, getUserLogs };
+
--- models/userModel.js
+++ models/userModel.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
 
-    userName : {
+    userName: {
         type: String,
         required: true,
         unique: [true, 'User already exists']
     },
 
-    exercises : {
-        type: Array,
-    }
+    exercises: [new mongoose.Schema({
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        date: {
+            type: String,
+        }
+    }, { _id: false })
+    ]
 });
 
 const UserModel = mongoose.model('user', UserSchema);
