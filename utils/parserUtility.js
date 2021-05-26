fd49a3feedda3aa8930640feda946fd9819c5ac5
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-26 21:18:58 +0100

    added form to get user info and logs

--- utils/parserUtility.js
+++ utils/parserUtility.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 function queryParser(queryFrom, queryTo, queryLimit) {
 
-    const from = queryFrom === undefined ? new Date(0).getTime() : new Date(queryFrom).getTime();
-    const to = queryTo === undefined ? new Date().getTime() : new Date(queryTo).getTime();
-    const limit = queryLimit === undefined ? 0 : parseInt(queryLimit);
+    const from = queryFrom === undefined || queryFrom === '' ? new Date(0).getTime() : new Date(queryFrom).getTime();
+    const to = queryTo === undefined || queryTo === '' ? new Date().getTime() : new Date(queryTo).getTime();
+    const limit = queryLimit === undefined || queryLimit === '' ? 0 : parseInt(queryLimit);
 
     return { from, to, limit };
 }
